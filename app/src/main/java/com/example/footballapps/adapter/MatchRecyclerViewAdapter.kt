7163464271ca2ba593690c935ce38bcc881a9767
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-08-26 18:38:44 +0700

    Change time zone into local time

--- app/src/main/java/com/example/footballapps/adapter/MatchRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/footballapps/adapter/MatchRecyclerViewAdapter.kt
@@ -9,10 +9,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.footballapps.R
 import com.example.footballapps.model.MatchItem
 import kotlinx.android.synthetic.main.item_match_data.view.*
+import java.lang.StringBuilder
 import java.text.SimpleDateFormat
 import java.util.*
 
-// todo: bikin class viewholder
 class MatchRecyclerViewAdapter(private val context : Context, private val matchList : List<MatchItem>) : RecyclerView.Adapter<MatchRecyclerViewAdapter.MatchViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchViewHolder =
         MatchViewHolder(LayoutInflater.from(context).inflate(R.layout.item_match_data, parent, false))
@@ -26,11 +26,10 @@ class MatchRecyclerViewAdapter(private val context : Context, private val matchL
     @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
     class MatchViewHolder(view : View) : RecyclerView.ViewHolder(view) {
 
-        // todo: bind item
         @SuppressLint("SimpleDateFormat")
         fun bindItem(matchItem : MatchItem) {
             itemView.league_item_name.text = matchItem.leagueName
-            itemView.league_item_match_week.text = matchItem.leagueMatchWeek
+            itemView.league_item_match_week.text = StringBuilder("Week ${matchItem.leagueMatchWeek}")
 
             val oldDateFormat = SimpleDateFormat("yyyy-MM-dd")
             val newDateFormat = SimpleDateFormat("dd MMM yyyy")
@@ -39,14 +38,12 @@ class MatchRecyclerViewAdapter(private val context : Context, private val matchL
 
             itemView.league_item_event_date.text = formattedDateEvent
 
-            // todo: time event
-//            val timeFormat = SimpleDateFormat("HH:mm:ssZZZZZ")
-//            timeFormat.timeZone = TimeZone.getTimeZone("UTC")
-//            val timeInDate : Date = timeFormat.parse(matchItem.timeEvent)
-//            timeFormat.timeZone = TimeZone.getDefault()
-//            val formattedTimeEvent = timeFormat.format(timeInDate)
-
-            itemView.league_item_event_time.text = matchItem.timeEvent // todo: modify this into gmt + 7 / local time
+            val timeFormat = SimpleDateFormat("HH:mm")
+            timeFormat.timeZone = TimeZone.getTimeZone("UTC")
+            val timeInDate : Date = timeFormat.parse(matchItem.timeEvent)
+            timeFormat.timeZone = TimeZone.getDefault()
+            val formattedTimeEvent = timeFormat.format(timeInDate)
+            itemView.league_item_event_time.text = formattedTimeEvent
 
             itemView.league_item_home_team_name.text = matchItem.homeTeamName
 
