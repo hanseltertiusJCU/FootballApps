662c4fd29a769574f73c2ff9710d7650f355365a
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-09-07 12:25:09 +0700

    Create layout for Activity with BottomNavigationView and MatchFragment

---
+++ app/src/main/java/com/example/footballapps/activity/FootballGameInfoActivity.kt
@@ -0,0 +1,13 @@
+package com.example.footballapps.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.footballapps.R
+
+class FootballGameInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_football_game_info)
+    }
+}
