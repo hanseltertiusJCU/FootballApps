6a1a1f4b940e4d555e2fc6fd9cd6580fca2250ca
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-09-27 14:44:47 +0700

    Implement instrumentation test based on current app structure

--- app/src/androidTest/java/com/example/footballapps/activity/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/footballapps/activity/MainActivityTest.kt
@@ -14,6 +14,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.example.footballapps.R
 import com.example.footballapps.espresso.EspressoIdlingResource
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.Matchers.anything
 import org.junit.After
 import org.junit.Before
@@ -34,120 +35,147 @@ class MainActivityTest {
 
     @Test
     fun testAppBehaviour() {
+        // todo: this is league list
         onView(withId(R.id.rv_league_list)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_league_list)).perform(
-            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
-                0,
-                click()
-            )
-        )
+        onView(withId(R.id.rv_league_list)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(4))
+        onView(withId(R.id.rv_league_list)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(4, click()))
 
+        // todo : this is league detail
         onView(withId(R.id.league_detail_layout)).check(matches(isDisplayed()))
-        onView(withId(R.id.action_match_schedule)).check(matches(isDisplayed()))
-        onView(withId(R.id.action_match_schedule)).perform(click())
+        onView(withId(R.id.view_pager_league_detail)).perform(swipeLeft())
+        onView(withId(R.id.rv_league_table)).check(matches(isDisplayed()))
+        Thread.sleep(500)
+        onView(withId(R.id.view_pager_league_detail)).perform(swipeLeft())
+        onView(withId(R.id.rv_league_teams)).check(matches(isDisplayed()))
+        Thread.sleep(500)
+        onView(withId(R.id.view_pager_league_detail)).perform(swipeLeft())
+        onView(withId(R.id.rv_league_match)).check(matches(isDisplayed()))
+        Thread.sleep(500)
 
-        onView(withId(R.id.rv_last_match)).check(matches(isDisplayed()))
-        onView(withId(R.id.last_match_league_spinner)).check(matches(withSpinnerText("English Premier League")))
-            .perform(click())
-        onData(anything()).atPosition(4).perform(click())
-        onView(withId(R.id.last_match_league_spinner)).check(matches(withSpinnerText("Spanish La Liga")))
-        onView(withId(R.id.rv_last_match)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_last_match)).perform(
-            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
-                10
-            )
-        )
-        onView(withId(R.id.rv_last_match)).perform(
-            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
-                10,
-                click()
-            )
-        )
-
-        onView(withId(R.id.layout_match_detail_data)).check(matches(isDisplayed()))
-        onView(withId(R.id.action_add_to_favorite)).check(matches(isDisplayed()))
-        onView(withId(R.id.action_add_to_favorite)).perform(click())
-        Espresso.pressBack()
-
-        onView(withId(R.id.view_pager_match_schedule)).perform(swipeLeft())
-        onView(withId(R.id.rv_next_match)).check(matches(isDisplayed()))
-        onView(withId(R.id.next_match_league_spinner)).check(matches(withSpinnerText("English Premier League")))
-            .perform(click())
-        onData(anything()).atPosition(3).perform(click())
-        onView(withId(R.id.next_match_league_spinner)).check(matches(withSpinnerText("Italian Serie A")))
-        onView(withId(R.id.rv_next_match)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_next_match)).perform(
-            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
-                7
-            )
-        )
-        onView(withId(R.id.rv_next_match)).perform(
-            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
-                7,
-                click()
-            )
-        )
-
-        onView(withId(R.id.layout_match_detail_data)).check(matches(isDisplayed()))
-        onView(withId(R.id.action_add_to_favorite)).check(matches(isDisplayed()))
-        onView(withId(R.id.action_add_to_favorite)).perform(click())
-        Espresso.pressBack()
-
-        onView(withId(R.id.action_search)).check(matches(isDisplayed()))
-        onView(withId(R.id.action_search)).perform(click())
-        onView(isAssignableFrom(EditText::class.java)).perform(
-            typeText("man united"),
-            pressImeActionButton()
-        )
-        onView(withId(R.id.rv_next_match)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_next_match)).perform(
-            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
-                5
-            )
-        )
-        onView(withId(R.id.rv_next_match)).perform(
-            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
-                5,
-                click()
-            )
-        )
-
-        onView(withId(R.id.layout_match_detail_data)).check(matches(isDisplayed()))
-        onView(withId(R.id.action_add_to_favorite)).check(matches(isDisplayed()))
-        onView(withId(R.id.action_add_to_favorite)).perform(click())
-        Espresso.pressBack()
-
-        onView(withId(R.id.view_pager_match_schedule)).perform(swipeRight())
-        onView(withId(R.id.rv_last_match)).check(matches(isDisplayed()))
-        onView(withText("Next Match")).perform(click())
-        onView(withId(R.id.next_match_parent_layout)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_next_match)).check(matches(isDisplayed()))
-        onView(withText("Last Match")).perform(click())
-        onView(withId(R.id.last_match_parent_layout)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_last_match)).check(matches(isDisplayed()))
-
+        // todo: this is match search feature on league detail
         onView(withId(R.id.action_search)).check(matches(isDisplayed()))
         onView(withId(R.id.action_search)).perform(click())
         onView(isAssignableFrom(EditText::class.java)).perform(
             typeText("barcelona"),
             pressImeActionButton()
         )
-        onView(withId(R.id.rv_last_match)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_league_match)).check(matches(isDisplayed()))
         onView(isAssignableFrom(EditText::class.java)).perform(clearText())
         onView(isAssignableFrom(EditText::class.java)).perform(
             typeText("liverpool"),
             pressImeActionButton()
         )
-        onView(withId(R.id.rv_last_match)).check(matches(isDisplayed()))
-        Espresso.pressBack()
-        onView(withId(R.id.rv_last_match)).check(matches(isDisplayed()))
 
+        onView(withId(R.id.rv_league_match)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_league_match)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(4))
+        onView(withId(R.id.rv_league_match)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(4, click()))
+
+        // todo: this is adding to favorites
+        onView(withId(R.id.layout_match_detail_data)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_add_to_favorite)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_add_to_favorite)).perform(click())
+        Espresso.pressBack()
+
+        // todo : this is trying to play with viewpager and click team item
+        onView(withId(R.id.view_pager_league_detail)).perform(swipeRight())
+        onView(withId(R.id.rv_league_teams)).check(matches(isDisplayed()))
+        Thread.sleep(500)
+        onView(withId(R.id.view_pager_league_detail)).perform(swipeLeft())
+        onView(withId(R.id.rv_league_match)).check(matches(isDisplayed()))
+        onView(withId(R.id.league_match_spinner)).check(matches(isDisplayed()))
+        Thread.sleep(500)
+        onView(withId(R.id.view_pager_league_detail)).perform(swipeRight())
+        onView(withId(R.id.rv_league_teams)).check(matches(isDisplayed()))
+        Thread.sleep(500)
+
+        // todo : this is trying to go to team detail
+        onView(withId(R.id.rv_league_teams)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(14))
+        onView(withId(R.id.rv_league_teams)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(14, click()))
+
+        // todo: action add to favorite team
+        onView(withId(R.id.team_detail_layout)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_add_to_favorite)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_add_to_favorite)).perform(click())
+        Espresso.pressBack()
+
+        // todo : search team feature in league detail
+        onView(withId(R.id.action_search)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_search)).perform(click())
+        onView(isAssignableFrom(EditText::class.java)).perform(typeText("man united"), pressImeActionButton())
+        onView(withId(R.id.rv_league_teams)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_league_teams)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        // todo : team detail layout
+        onView(withId(R.id.team_detail_layout)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_add_to_favorite)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_add_to_favorite)).perform(click())
+
+        // todo : playing with tablayout matches
+        onView(allOf(withText("Matches"), isDescendantOfA(withId(R.id.tab_layout_team_detail)))).perform(
+            click())
+        onView(withId(R.id.rv_team_match)).check(matches(isDisplayed()))
+        Thread.sleep(500)
+        onView(withId(R.id.team_match_spinner)).check(matches(withSpinnerText("Last Matches")))
+            .perform(click())
+        onData(anything()).atPosition(1).perform(click())
+        onView(withId(R.id.team_match_spinner)).check(matches(withSpinnerText("Next Matches")))
+        onView(withId(R.id.rv_team_match)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_team_match)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        onView(withId(R.id.layout_match_detail_data)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_add_to_favorite)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_add_to_favorite)).perform(click())
+        Espresso.pressBack()
+
+        // todo : search feature in team match
+        onView(withId(R.id.action_search)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_search)).perform(click())
+        onView(isAssignableFrom(EditText::class.java)).perform(
+            typeText("juventus"),
+            pressImeActionButton()
+        )
+        onView(withId(R.id.rv_team_match)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_team_match)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(4))
+        onView(withId(R.id.rv_team_match)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(4, click()))
+
+        onView(withId(R.id.layout_match_detail_data)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_add_to_favorite)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_add_to_favorite)).perform(click())
+        Espresso.pressBack()
+
+        // todo : players in team
+        Espresso.pressBack()
+        Espresso.pressBack()
+        onView(withId(R.id.rv_team_match)).check(matches(isDisplayed()))
+        onView(allOf(withText("Players"), isDescendantOfA(withId(R.id.tab_layout_team_detail)))).perform(
+            click())
+        onView(withId(R.id.rv_team_players)).check(matches(isDisplayed()))
+        Thread.sleep(500)
+        onView(withId(R.id.rv_team_players)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(20))
+        onView(withId(R.id.rv_team_players)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(20, click()))
+
+        onView(withId(R.id.layout_player_detail_data)).check(matches(isDisplayed()))
+        onView(withId(R.id.scroll_view_player_detail)).perform(swipeUp())
+        Thread.sleep(1000)
+        onView(withId(R.id.scroll_view_player_detail)).perform(swipeDown())
+        Thread.sleep(1000)
+        Espresso.pressBack() // to team detail
+        Espresso.pressBack() // to league detail
+
+        // to disable searchview
+        Espresso.pressBack()
+        Espresso.pressBack()
+
+        // to go to main activity
+        Espresso.pressBack()
+
+        // todo : favorite match feature in main activity
         onView(withId(R.id.menu_item_favorite)).perform(click())
         onView(withId(R.id.rv_favorite_match)).check(matches(isDisplayed()))
         onView(withId(R.id.action_search)).check(matches(isDisplayed()))
         onView(withId(R.id.action_search)).perform(click())
         onView(isAssignableFrom(EditText::class.java)).perform(
-            typeText("man united"),
+            typeText("liverpool"),
             pressImeActionButton()
         )
         onView(withId(R.id.rv_favorite_match)).check(matches(isDisplayed()))
@@ -158,15 +186,43 @@ class MainActivityTest {
             )
         )
 
+        // todo : remove from favorite
         onView(withId(R.id.layout_match_detail_data)).check(matches(isDisplayed()))
         onView(withId(R.id.action_add_to_favorite)).check(matches(isDisplayed()))
         onView(withId(R.id.action_add_to_favorite)).perform(click())
         Espresso.pressBack()
 
         Espresso.pressBack()
+        Espresso.pressBack()
 
-        onView(withId(R.id.menu_item_match_schedule)).perform(click())
-        onView(withId(R.id.rv_last_match)).check(matches(isDisplayed()))
+        // todo : team feature in favorite
+        onView(allOf(withText("Teams"), isDescendantOfA(withId(R.id.tab_layout_favorite_fragment)))).perform(
+            click())
+        onView(withId(R.id.rv_favorite_team)).check(matches(isDisplayed()))
+        Thread.sleep(500)
+
+        onView(withId(R.id.action_search)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_search)).perform(click())
+        onView(isAssignableFrom(EditText::class.java)).perform(
+            typeText("real madrid"),
+            pressImeActionButton()
+        )
+        onView(withId(R.id.rv_favorite_team)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_favorite_team)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+
+        onView(withId(R.id.team_detail_layout)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_add_to_favorite)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_add_to_favorite)).perform(click())
+        Espresso.pressBack()
+
+        Espresso.pressBack()
+        onView(withId(R.id.menu_item_leagues)).perform(click())
+        onView(withId(R.id.rv_league_list)).check(matches(isDisplayed()))
 
     }
 
